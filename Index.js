f3dcd09556e88221e049e71a01ede82c0f59ea03
Rupam Barui <98939183+Trident09@users.noreply.github.com>
2022-09-11 08:14:19 +0000

    Added working buttons and authorisation for admins via prompt

---
+++ Index.js
@@ -0,0 +1,34 @@
+function admin() 
+{
+  var question = prompt("Administrative password");
+  var reponse = "12345";
+  if (question === reponse) 
+  {
+    authorize();
+  } 
+  else 
+  {
+    alert("enter correct password");
+  }
+  function authorize() 
+  {
+    window.open("Admin.html");
+  }
+}
+function student() 
+{
+  var questions = prompt("are you a student ? (Y/N)");
+  var responses = "Y";
+  if (questions === responses)
+  {
+    admit();
+  }
+  else
+  {
+    alert("only for students");
+  }
+  function admit()
+  {
+    window.open("Students.html");
+  }
+}
